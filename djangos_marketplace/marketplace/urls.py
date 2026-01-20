8a6f86746c5df5df542dd51bb65d8329e2b3690a
Aaron Richards <aaronrichards46@gmail.com>
2026-01-20 09:23:47 +0000

    Configure marketplace URL routing
    - Setup routes for home, market list, item detail
    - Add routes for create, edit, delete items

--- djangos_marketplace/marketplace/urls.py
+++ djangos_marketplace/marketplace/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='market-index'),
-
+    path('', views.HomeView.as_view(), name='home'),
+    path('market/', views.MarketListView.as_view(), name='market'),
+    path('item/<int:pk>/', views.ItemDetailView.as_view(), name='item_detail'),
+    path('item/new/', views.ItemCreateView.as_view(), name='item_create'),
+    path('item/<int:pk>/edit/', views.ItemUpdateView.as_view(), name='item_edit'),
+    path('item/<int:pk>/delete/', views.ItemDeleteView.as_view(), name='item_delete'),
 ]
